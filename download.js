b74844e747fb7e7694952ce453e60023ad877335
dinhlvtlu <dinhlv@Techmasters-iMac-7.local>
2016-04-15 14:54:51 +0700

    sup return

--- download.js
+++ download.js
@@ -5,12 +5,13 @@ const progressBar = require('progress');
 var processData = process.argv;
 var url = processData[2];
 
-
+var arrEx = ['png','jpeg','jpg','gif','tiff'];
 var totalBytes;
 var bar;
 
+downloadImage(url);
+
 function Contain(ex){
-    var arrEx = ['png','jpeg','jpg','gif','tiff'];
     for(var i = 0 ; i < arrEx.length ; i++)
     {
         if(ex == arrEx[i])
@@ -22,52 +23,54 @@ function Contain(ex){
 };
 
 
+function downloadImage(url){
+    if(!url)
+    {
+            return console.log("URL null...!");
 
-if(!url)
-{
-    console.log("URL null...!")
-    return;
-}
-else {
-    var ex = url.split('.');
-    ex = ex.pop();
-    var downloadIMG = request.get(url);
-    console.time('download');
-    downloadIMG.on('error', function (err) {
-        console.log("Download Error ", err);
-    });
-    downloadIMG.on('response', function (response) {
-        if (response.statusCode != 200) {
-            console.log("Error " + response.statusCode);
-            return;
+    }
+    else {
+            var ex = url.split('.');
+            ex = ex.pop();
+            var downloadIMG = request.get(url);
+            console.time('download');
+            downloadIMG.on('error', function (err) {
+                return console.log("Download Error ", err);
+            });
+            downloadIMG.on('response', function (response) {
+                if (response.statusCode != 200) {
+                    console.log("Error " + response.statusCode);
+                    return;
+                }
+                else {
+                    if (Contain(ex) === true) {
+                        totalBytes = parseInt(response.headers['content-length'], 10);
+                        console.log("Status Code:  " + response.statusCode);
+                        bar = new progressBar('downloading [:bar] :percent :etas', {
+                            complete: '*',
+                            incomplete: ' ',
+                            width: 50,
+                            total: totalBytes
+                        });
+                        downloadIMG.on('data', function (chunk) {
+                            bar.tick(chunk.length);
+                        });
+                        downloadIMG.pipe(fs.createWriteStream('Image.' + ex))
+                            .on('finish', function () {
+                                return console.timeEnd('download');
+                            })
+                            .on('error', function (err) {
+                                return console.log("Error: " + err);
+                            });
+                    }
+                    else
+                    {
+                        return console.log("ULR khong dan toi file anh");
+
+                    }
+                }
+            });
         }
-        else {
-            if (Contain(ex) === true) {
-                totalBytes = parseInt(response.headers['content-length'], 10);
-                console.log("Status Code:  " + response.statusCode);
-                bar = new progressBar('downloading [:bar] :percent :etas', {
-                    complete: '*',
-                    incomplete: ' ',
-                    width: 50,
-                    total: totalBytes
-                });
-                downloadIMG.on('data', function (chunk) {
-                    bar.tick(chunk.length);
-                });
-                downloadIMG.pipe(fs.createWriteStream('Image.' + ex))
-                    .on('finish', function () {
-                        console.timeEnd('download');
-                    })
-                    .on('error', function (err) {
-                        console.log("Error: " + err);
-                    });
-            }
-            else
-            {
-                console.log("ULR khong dan toi file anh")
-            }
-        }
-    });
 }
 
 
